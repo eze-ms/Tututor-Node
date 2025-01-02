6b497fb609455eeeb98d94679fa52042ab6f76f4
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2025-01-02 11:35:51 +0100

    Añadir log para verificar DATABASE_URL

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: 'variables.env' });
 
+// Verificar el valor de la variable de entorno
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
