18eb9da6dfe60f3e1a35119b66dbcf9a996f08e6
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2025-01-03 11:38:58 +0100

    varios cambios en el codigo

--- config/db.js
+++ config/db.js
@@ -1,36 +1,36 @@
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config({ path: 'variables.env' });
-
-// // Verificar el valor de la variable de entorno
-// console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false,
-//         },
-//     },
-//     logging: false,
-// });
-
-// module.exports = sequelize;
-
 const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: 'variables.env' });
 
-const sequelize = new Sequelize(
-    process.env.BD_NOMBRE, 
-    process.env.BD_USER, 
-    process.env.BD_PASS, 
-    {
-        host: process.env.BD_HOST,
-        port: process.env.ND_PORT,
-        dialect: 'postgres',
-        logging: false
-    }
-);
+// Verificar el valor de la variable de entorno
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+    logging: false,
+});
 
 module.exports = sequelize;
+
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config({ path: 'variables.env' });
+
+// const sequelize = new Sequelize(
+//     process.env.BD_NOMBRE, 
+//     process.env.BD_USER, 
+//     process.env.BD_PASS, 
+//     {
+//         host: process.env.BD_HOST,
+//         port: process.env.ND_PORT,
+//         dialect: 'postgres',
+//         logging: false
+//     }
+// );
+
+// module.exports = sequelize;
 
