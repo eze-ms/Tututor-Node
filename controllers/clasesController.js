cec641c59064d69c4ab11f9c1e517c468c724bba
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2025-01-26 21:40:04 +0100

    add key aws

--- controllers/clasesController.js
+++ controllers/clasesController.js
@@ -15,10 +15,11 @@ const slugify = require('slugify');
 
 // Configurar S3
 const s3 = new AWS.S3({
-    accessKeyId: process.env.REDACTED,
-    secretAccessKey: process.env.REDACTED,
-    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
+    region: process.env.AWS_REGION,  
 });
+
 
 //! Configuración de Multer para Subida de Imágenes
 const configuracionMulter = multer({
--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -12,10 +12,11 @@ const homeController = require('./homeController');
 
 // Configurar S3
 const s3 = new AWS.S3({
-    accessKeyId: process.env.REDACTED,
-    secretAccessKey: process.env.REDACTED,
-    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,  
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
+    region: process.env.AWS_REGION,  
 });
+
 
 
 //! Mostrar el formulario de crear cuenta
